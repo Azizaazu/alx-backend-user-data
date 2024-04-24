1af95af3eebbbcd78e8e8964570b236601042e84
Azizaazu <aynaou.aziza98@gmail.com>
2024-04-24 12:41:53 -0700

    update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,7 +6,9 @@ User model for SQLAlchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class User(Base):
     """
